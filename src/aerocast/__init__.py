10947c4532e5dccc23ffeb9f320ce8f9d70f4b57
Hirotaka Kanda <pursue0531.h@gmail.com>
2026-01-27 13:18:52 +0900

    呼び出し修正

--- src/aerocast/__init__.py
+++ src/aerocast/__init__.py
@@ -1,1 +1,40 @@
-# AeroCast Engine Package
+"""
+AeroCast Engine Package
+
+天気情報を取得し、データ化、予測の回答を行うパッケージ
+"""
+
+# メインのエージェント関数
+from .agent import run_agent
+
+# データモデル
+from .models import (
+    WeatherContext,
+    WeatherResult,
+    UmbrellaDecision,
+    WindDecision,
+    ComfortDecision,
+)
+
+# エラークラス
+from .error import (
+    UserFacingError,
+    CityNotFoundError,
+    WeatherAPIError,
+)
+
+# 公開APIを明示的に定義
+__all__ = [
+    # メイン関数
+    "run_agent",
+    # データモデル
+    "WeatherContext",
+    "WeatherResult",
+    "UmbrellaDecision",
+    "WindDecision",
+    "ComfortDecision",
+    # エラークラス
+    "UserFacingError",
+    "CityNotFoundError",
+    "WeatherAPIError",
+]
